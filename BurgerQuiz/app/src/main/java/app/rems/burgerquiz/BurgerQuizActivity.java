edee90eff0743487645fd48cfb0db298f71b748b
Remy Baroukh <remsdu84@gmail.com>
2015-04-01 00:42:07 +0100

    Nettoyage des choses inutiles auto genere!

--- BurgerQuiz/app/src/main/java/app/rems/burgerquiz/BurgerQuizActivity.java
+++ BurgerQuiz/app/src/main/java/app/rems/burgerquiz/BurgerQuizActivity.java
@@ -2,25 +2,12 @@ package app.rems.burgerquiz;
 
 import app.rems.burgerquiz.game.BurgerQuiz;
 import app.rems.burgerquiz.game.BurgerVariables;
-import app.rems.burgerquiz.util.SystemUiHider;
-
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
-
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class BurgerQuizActivity extends Activity {
 
     /**
@@ -35,11 +22,11 @@ public class BurgerQuizActivity extends Activity {
 
         setContentView(R.layout.activity_burger_quiz);
 
+        BurgerVariables.burgerQuiz = new BurgerQuiz();
+
         setUpUi();
         ivMayo = (ImageView) findViewById(R.id.ivMayo);
         ivKetchup = (ImageView) findViewById(R.id.ivKetchup);
-
-        BurgerVariables.burgerQuiz = new BurgerQuiz();
 
         ivMayo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,16 +41,19 @@ public class BurgerQuizActivity extends Activity {
             }
         });
 
-
     }
 
     private void setUpUi() {
+
+
+        //Animated Burger
         ImageView ivBurger = (ImageView) findViewById(R.id.ivBurger);
         ivBurger.setBackgroundResource(R.drawable.burger_animation);
         AnimationDrawable ivBurgerAnimation = (AnimationDrawable) ivBurger.getBackground();
         ivBurgerAnimation.start();
-        getActionBar().hide();
 
+
+        getActionBar().hide();
 
     }
 
